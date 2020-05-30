0989db3cb4768341eb17a6a13a24f2e8a80ba074
PhilippScheller <philipp-scheller@gmx.de>
2020-05-30 12:41:45 +0200

    runAppLocal

---
+++ R/runAppLocal.R
@@ -0,0 +1,15 @@
+#' Run Shiny App on local machine
+#'
+#' This function helps to deploy the shiny app in an easy way
+#' on the local machine.
+#'
+#' @import shiny
+#'
+#' @export
+
+runAppLocal = function() {
+
+  appDir <- system.file("shinyApp", package = "visualDescent")
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
