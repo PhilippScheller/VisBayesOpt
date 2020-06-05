426f84a5526336e1a5e53d266020a95cab3679f1
PhilippScheller <philipp-scheller@gmx.de>
2020-06-05 19:09:31 +0200

    fix bug MboPlot

--- R/MboPlot.R
+++ R/MboPlot.R
@@ -45,7 +45,7 @@ MboPlot = R6Class(
       self$opt_state = assert_class(opt_state, "OptState")
       self$opt_path = assert_class(opt_state$opt.path, "OptPath")
       self$param_set = assert_class(opt_state$opt.path$par.set, "ParamSet")
-      self$mbo_control = assert_class(opt_state$opt.problem$mbo_control, "MBOControl")
+      self$mbo_control = assert_class(opt_state$opt.problem$mbo.control, "MBOControl")
     },
     #' @description
     #' Sets parameter values
