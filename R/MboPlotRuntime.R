8b641918b23a74289f583057896c07a1d2c63ac2
PhilippScheller <philipp-scheller@gmx.de>
2020-06-18 11:01:39 +0200

    MboPlotRuntime

--- R/MboPlotRuntime.R
+++ R/MboPlotRuntime.R
@@ -4,6 +4,7 @@
 #'
 #' @import checkmate
 #' @import mlrMBO
+#' @import BBmisc
 #' @import ParamHelpers
 #'
 #' @importFrom R6 R6Class
@@ -20,17 +21,31 @@ MboPlotRuntime = R6Class(
     #' @description
     #' Plots minimal value of model (y) of mbo run over the iterations.
     #'
+    #' @param highlight_iter (\code{integer(1) | NULL})\cr
+    #' Specifies the iteration to be highlighted. The default \code{NULL} does not highlight any iteration.
+    #'
     #' @return ([ggplot]).
-    plot = function() {
+    plot = function(highlight_iter = NULL) {
+      if (!is.null(highlight_iter)) highlight_iter = assertClass(highlight_iter, "numeric")
       df_extra = convertListOfRowsToDataFrame(self$opt_state$opt.path$env$extra)
       df_time = df_extra[colnames(df_extra) %in% c("train.time", "propose.time")]
       df_time$total.time = df_time$train.time + df_time$propose.time
+
+      if (!is.null(highlight_iter)) {
+        if(highlight_iter < 0 | highlight_iter > nrow(df_time)) {
+          stop("`highlight_iter` exceeds number of iterations from mbo run (n=", nrow(df_time), ")")
+        }
+      }
 
       df_time_long = wideToLong(df_time, 0)
 
       gg = ggplot(df_time_long, aes(x = rep(seq(1, nrow(df_time)), nrow(df_time_long)/nrow(df_time))
                                     , y = Value, color = Param))
       gg = gg + geom_ribbon(aes(ymin = 0, ymax = Value, fill = Param), alpha = .2)
+      if (!is.null(highlight_iter)) {
+        gg = gg + geom_line(data = data.frame(x = c(highlight_iter, highlight_iter), y = c(0, Inf)),
+                               mapping = aes(x = x, y = y), col = "black", lty = "dashed")
+      }
       gg = gg + geom_line(na.rm = TRUE)
       gg = gg + theme(legend.title=element_blank())
       gg = gg + xlab("Iteration")
